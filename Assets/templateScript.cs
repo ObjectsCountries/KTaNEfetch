12e6d72449046e055608fa6c2ed142f7fb9d2876
Kasra Moayedi <kasramoayedi@protonmail.com>
2024-12-29 13:43:16 -0800

    Initial commit

---
+++ Assets/templateScript.cs
@@ -0,0 +1,27 @@
+﻿using wawa.Modules;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class templateScript : ModdedModule
+{
+
+    public KMSelectable thing;
+
+    //place module stuff like buttons here
+
+    internal bool moduleSolved;
+
+    protected override void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+}
